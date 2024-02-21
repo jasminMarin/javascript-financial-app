2cb881001cc07facca85a798a22c71e1d93b0009
jasminMarin <jasmin.marin2021@gmail.com>
2024-02-21 09:03:50 +0100

    mungu, on sar-g format hiine

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ var uiController = (function() {
         var count = 1;
         for (var i = 0; i < x.length; i++) {
             y = y + x[i];
-            if (count % 3 === 0) y= y + ",";
+            if (count % 3 === 0) y = y + ",";
             count ++;
         }
         var z = y.split("").reverse().join(""); 
-        if (z[0] === ",") z = z.substring(1, z.length -1);
-        if (type === "inc") z = "+ " + z;
-         else z = "- " + z;
+        if (z[0] === ",") z = z.substring(1, z.length - 1);
+        if (type === "inc") z = "+" + z;
+         else z = "-" + z;
          return z;
     };
     return {
@@ -79,9 +79,12 @@ var uiController = (function() {
         },
 
         tusviigUzuuleh: function(tusuv) {
-            document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
-            document.querySelector(DOMstrings.incomeLabel).textContent = tusuv.totalInc;
-            document.querySelector(DOMstrings.expenseLabel).textContent = tusuv.totalExp;
+            var type;
+            if (tusuv.tusuv > 0) type = "inc";
+            else type = "exp";
+            document.querySelector(DOMstrings.tusuvLabel).textContent = formatMoney(tusuv.tusuv, type);
+            document.querySelector(DOMstrings.incomeLabel).textContent = formatMoney(tusuv.totalInc, "inc");
+            document.querySelector(DOMstrings.expenseLabel).textContent = formatMoney(tusuv.totalExp, "exp");
             if(tusuv.huvi > 0) {
             document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi + "%";
             } else {
